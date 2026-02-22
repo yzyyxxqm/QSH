06b40642d55130135e444c5bba47480ba2398c50
Ladbaby <1390265970@qq.com>
2026-02-22 11:03:13 +0800

    feat(models):MOIRAI imputation

--- models/MOIRAI.py
+++ models/MOIRAI.py
@@ -71,7 +71,9 @@ class Model(nn.Module):
         if self.configs.task_name in ["long_term_forecast", "short_term_forecast"]:
             assert (self.seq_len + self.pred_len) % self.patch_len == 0, f"{self.seq_len+self.pred_len=} should be divisible by {self.patch_len=}"
             self.n_patch_all: int = math.ceil(configs.seq_len / configs.patch_len) + math.ceil(configs.pred_len / configs.patch_len) # pad pred_len to times of patch_len
-
+        elif self.configs.task_name == "imputation":
+            assert self.seq_len % self.patch_len == 0, f"{self.seq_len=} should be divisible by {self.patch_len=}"
+            self.n_patch_all: int = math.ceil(configs.seq_len / configs.patch_len) # pad pred_len to times of patch_len
 
     def forward(
         self,
@@ -93,7 +95,7 @@ class Model(nn.Module):
         if x_mask is None:
             x_mask = torch.ones_like(x, device=x.device, dtype=x.dtype)
         if y is None:
-            if self.configs.task_name in ["short_term_forecast", "long_term_forecast"]:
+            if self.configs.task_name in ["short_term_forecast", "long_term_forecast", "imputation"]:
                 logger.warning(f"y is missing for the model input. This is only reasonable when the model is testing flops!")
             y = torch.ones((BATCH_SIZE, Y_LEN, ENC_IN), dtype=x.dtype, device=x.device)
         if y_mark is None:
@@ -120,6 +122,23 @@ class Model(nn.Module):
                     "pred": self.revert_rearrange_and_pad_multivariate_tensor(tensor=output.mean, batch_size=BATCH_SIZE),
                     "true": torch.cat([x, y], dim=1),
                     "mask": torch.cat([x_mask, y_mask], dim=1),
+                    "loss": loss
+                }
+            else:
+                f_dim = -1 if self.configs.features == 'MS' else 0
+                PRED_LEN = y.shape[1]
+                return {
+                    "pred": self.revert_rearrange_and_pad_multivariate_tensor(tensor=output.mean, batch_size=BATCH_SIZE)[:, -PRED_LEN:, f_dim:],
+                    "true": y,
+                    "mask": y_mask,
+                    "loss": loss
+                }
+        elif self.configs.task_name == "imputation":
+            if exp_stage == "train":
+                return {
+                    "pred": self.revert_rearrange_and_pad_multivariate_tensor(tensor=output.mean, batch_size=BATCH_SIZE),
+                    "true": x + y,
+                    "mask": x_mask + y_mask,
                     "loss": loss
                 }
             else:
@@ -207,18 +226,9 @@ class Model(nn.Module):
         PATCH_LEN_MAX = self.patch_len_padded
         PRED_LEN = y.shape[1]
         L = SEQ_LEN + PRED_LEN
-        variate_id = repeat(
-            torch.arange(ENC_IN, device=x.device),
-            "ENC_IN -> BATCH_SIZE L ENC_IN",
-            BATCH_SIZE=BATCH_SIZE,
-            L=L
-        )
 
         BATCH_SIZE_NEW = BATCH_SIZE * ENC_IN
         L_NEW = self.n_patch_all
-
-        x_padding = torch.zeros_like(y)
-        y_padding = torch.zeros_like(x)
 
         sample_id = repeat(
             sample_ID, 
@@ -227,31 +237,73 @@ class Model(nn.Module):
             L_NEW=L_NEW
         ).int()
 
-        observed_mask = self.rearrange_and_pad_multivariate_tensor(torch.cat([x_mask, y_mask], dim=1)).bool()
-        prediction_mask = self.rearrange_and_pad_multivariate_tensor(torch.cat([y_padding, y_mask], dim=1)).sum(dim=-1).bool()
-        variate_id = self.rearrange_and_pad_multivariate_tensor(variate_id).sum(dim=-1).int()
-
-        output = self.MoiraiModule(
-            target=self.rearrange_and_pad_multivariate_tensor(torch.cat([x, x_padding], dim=1)),
-            observed_mask=observed_mask,
-            sample_id=sample_id,
-            time_id=self.rearrange_and_pad_multivariate_tensor(
-                repeat(
-                    torch.cat([x_mark[:, :, 0], y_mark[:, :, 0]], dim=1),
-                    "BATCH_SIZE L -> BATCH_SIZE L ENC_IN",
-                    ENC_IN=ENC_IN
-                )
-            ).sum(dim=-1).int(),
-            variate_id=variate_id,
-            prediction_mask=prediction_mask,
-            patch_size=torch.full(
-                (BATCH_SIZE_NEW, L_NEW), 
-                fill_value=PATCH_LEN, 
-                device=x.device
+        if self.configs.task_name in ["short_term_forecast", "long_term_forecast"]:
+            variate_id = repeat(
+                torch.arange(ENC_IN, device=x.device),
+                "ENC_IN -> BATCH_SIZE L ENC_IN",
+                BATCH_SIZE=BATCH_SIZE,
+                L=L
             )
-        )
+            x_padding = torch.zeros_like(y)
+            y_padding = torch.zeros_like(x)
 
-        target = self.rearrange_and_pad_multivariate_tensor(torch.cat([y_padding, y], dim=1))
+            observed_mask = self.rearrange_and_pad_multivariate_tensor(torch.cat([x_mask, y_mask], dim=1)).bool()
+            prediction_mask = self.rearrange_and_pad_multivariate_tensor(torch.cat([y_padding, y_mask], dim=1)).sum(dim=-1).bool()
+            variate_id = self.rearrange_and_pad_multivariate_tensor(variate_id).sum(dim=-1).int()
+
+            output = self.MoiraiModule(
+                target=self.rearrange_and_pad_multivariate_tensor(torch.cat([x, x_padding], dim=1)),
+                observed_mask=observed_mask,
+                sample_id=sample_id,
+                time_id=self.rearrange_and_pad_multivariate_tensor(
+                    repeat(
+                        torch.cat([x_mark[:, :, 0], y_mark[:, :, 0]], dim=1),
+                        "BATCH_SIZE L -> BATCH_SIZE L ENC_IN",
+                        ENC_IN=ENC_IN
+                    )
+                ).sum(dim=-1).int(),
+                variate_id=variate_id,
+                prediction_mask=prediction_mask,
+                patch_size=torch.full(
+                    (BATCH_SIZE_NEW, L_NEW), 
+                    fill_value=PATCH_LEN, 
+                    device=x.device
+                )
+            )
+
+            target = self.rearrange_and_pad_multivariate_tensor(torch.cat([y_padding, y], dim=1))
+        elif self.configs.task_name in ["imputation"]:
+            variate_id = repeat(
+                torch.arange(ENC_IN, device=x.device),
+                "ENC_IN -> BATCH_SIZE L ENC_IN",
+                BATCH_SIZE=BATCH_SIZE,
+                L=SEQ_LEN
+            )
+            observed_mask = self.rearrange_and_pad_multivariate_tensor(x_mask + y_mask).bool()
+            prediction_mask = self.rearrange_and_pad_multivariate_tensor(y_mask).sum(dim=-1).bool()
+            variate_id = self.rearrange_and_pad_multivariate_tensor(variate_id).sum(dim=-1).int()
+
+            output = self.MoiraiModule(
+                target=self.rearrange_and_pad_multivariate_tensor(x),
+                observed_mask=observed_mask,
+                sample_id=sample_id,
+                time_id=self.rearrange_and_pad_multivariate_tensor(
+                    repeat(
+                        x_mark[:, :, 0],
+                        "BATCH_SIZE L -> BATCH_SIZE L ENC_IN",
+                        ENC_IN=ENC_IN
+                    )
+                ).sum(dim=-1).int(),
+                variate_id=variate_id,
+                prediction_mask=prediction_mask,
+                patch_size=torch.full(
+                    (BATCH_SIZE_NEW, L_NEW), 
+                    fill_value=PATCH_LEN, 
+                    device=x.device
+                )
+            )
+
+            target = self.rearrange_and_pad_multivariate_tensor(y)
 
         loss = self.loss_fn(
             pred=output,
